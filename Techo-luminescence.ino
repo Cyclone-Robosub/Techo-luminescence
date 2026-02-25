c41cf3b5bb37b4034d15b32cd83537c5665a2d58
jdpieck <jdpieck@ucdavis.edu>
2026-02-24 20:07:24 -0800

    reorganized functions, added new twinkle

--- Techo-luminescence.ino
+++ Techo-luminescence.ino
@@ -1,25 +1,70 @@
- #include <FastLED.h>
- #define NUM_LEDS 20
- #define LED_PIN 2
- #define CYCLE_RATE 200
+#include <FastLED.h>
+#define NUM_LEDS 20
+#define LED_PIN 2
+#define CYCLE_RATE 100
+#define DEFAULT_COLOR CRGB::White
 
- CRGB leds[NUM_LEDS];
+CRGB leds[NUM_LEDS];
 
- void setup () {
+void setup () {
   FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
   FastLED.setBrightness(30);
   delay(2000);
- }
+}
 
- void dim_leds () {
+void dim_leds () {
   for (auto &i:leds) {
     i.fadeLightBy( 100 );
   }
+
   FastLED.show();
- }
+}
 
- void loop () {
+void forward_animation (fl::u32 color = DEFAULT_COLOR, int cycle_rate = CYCLE_RATE) {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = color;
+    FastLED.show();
+    delay(cycle_rate);
+    dim_leds();
+  } 
+}
 
+void backward_animation (fl::u32 color = DEFAULT_COLOR, int cycle_rate = CYCLE_RATE) {
+  for (int i = NUM_LEDS-1; i >= 0; i--) {
+    leds[i] = color;
+    FastLED.show();
+    delay(cycle_rate);
+    dim_leds();
+  }
+}
+
+void bouncing_animation (fl::u32 color, int cycle_rate) {
+  forward_animation(color, cycle_rate);
+  backward_animation(color, cycle_rate);
+}
+
+void twinkle_animation (int spacing = 4, fl::u32 color = DEFAULT_COLOR, int cycle_rate = CYCLE_RATE*2) {
+  int index = 0;  
+  int tracker = 0;
+  int offset = 0; 
+
+  for (int i = 0; i < NUM_LEDS; i++) {
+    tracker = i * spacing;
+    offset = floor(tracker / NUM_LEDS);
+    index = tracker % NUM_LEDS + offset; 
+    leds[index] = color;
+    FastLED.show();
+    delay(cycle_rate);
+    dim_leds();
+  } 
+}
+
+void loop () {
+  
+  // forward_animation();
+  // backward_animation();
+  // bouncing_animation(CRGB::Blue, 50);
+  twinkle_animation();
 
   // leds[0] = CRGB::Red;
   // leds[0].maximizeBrightness();
@@ -43,24 +88,7 @@
   // leds[1] = CRGB::Green;
   // leds[2] = CRGB::Blue;
 
-  for (int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CRGB::Red;
-    FastLED.show();
-    delay(CYCLE_RATE);
-    dim_leds();
-    // leds[i] %= 192;
-  //  leds[i] = CRGB::Black;
-  } 
 
-  for (int i = NUM_LEDS-1; i >= 0; i--) {
-    leds[i] = CRGB::Blue;
-    FastLED.show();
-    delay(CYCLE_RATE);
-  dim_leds();
-
-  //  leds[i] = CRGB::Black;
-   
-  }
 
   // FastLED.show();
  }
